08f646a2e3d2242169efbe3c4d67bf5734b2b770
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2018-12-11 09:09:57 -0500

    Authentication service, interceptor and  component created

---
+++ src/app/shared/services/authentication.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { shareReplay, tap } from 'rxjs/operators';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.http.post('/api/login', { email, password }).pipe(
+      tap(r => this.setSession),
+      shareReplay()
+    );
+  }
+
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresAt, 'second');
+
+    localStorage.setItem('id_token', authResult.id_token);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+}
